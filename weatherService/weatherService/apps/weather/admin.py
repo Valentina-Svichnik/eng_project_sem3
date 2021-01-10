b9252f7495562e15487e2fe620876880de00e2cc
Valentina Svichnik <svichnikvalentina@gmail.com>
2021-01-10 22:44:18 +0300

    test seeding the database

--- weatherService/weatherService/apps/weather/admin.py
+++ weatherService/weatherService/apps/weather/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Country, City, District, Temperature, Humidity, Pressure, Wind, Precipitation, Precipitation_icon, Cloudy, Cloudy_icon, New, Day, User, Advertice
+from .models import Country, City, District, Temperature, Humidity, Pressure, Wind, Precipitation, Precipitation_icon, Cloud, Cloudy_icon, New, Day, User, Advertice
 
 admin.site.register(Country)
+# class CountryAdmin(admin.ModelAdmin):
+#     list_display = ['name', 'population', 'square']
 admin.site.register(City)
 admin.site.register(District)
 admin.site.register(Temperature)
@@ -11,7 +13,7 @@ admin.site.register(Pressure)
 admin.site.register(Wind)
 admin.site.register(Precipitation)
 admin.site.register(Precipitation_icon)
-admin.site.register(Cloudy)
+admin.site.register(Cloud)
 admin.site.register(Cloudy_icon)
 admin.site.register(New)
 admin.site.register(Day)
--- weatherService/weatherService/apps/weather/models.py
+++ weatherService/weatherService/apps/weather/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# Country, City, District, Temperature, Humidity, Pressure, Wind, Precipitation, Precipitation_icon, Cloudy, Cloudy_icon, New, Day, User, Advertice
+
 class Country(models.Model):
     name = models.CharField('название страны', max_length=100)
     popolation = models.CharField('население страны', max_length=100)
@@ -13,7 +15,10 @@ class City(models.Model):
 
     name = models.CharField('название города', max_length=100)
     popolation = models.CharField('население города', max_length=100)
-    square = models.CharField('площадь города', max_length=100)    
+    square = models.CharField('площадь города', max_length=100)  
+
+    def __str__(self):
+        return self.name  
 
 class District(models.Model):
     id_city = models.ForeignKey(City, on_delete = models.CASCADE)
@@ -22,45 +27,60 @@ class District(models.Model):
     popolation = models.CharField('население района', max_length=100)
     square = models.CharField('площадь района', max_length=100)    
 
+    def __str__(self):
+        return self.name
+
 class Temperature (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     t_morning = models.IntegerField('температура утром')
     t_day = models.IntegerField('температура днем')
     t_evening = models.IntegerField('температура вечером')
     t_night = models.IntegerField('температура ночью')
     t_average = models.IntegerField('средняя температура за день')
 
+    # def __str__(self):
+    #     return (self.id_district, self.date)
+
 class Humidity (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     h_morning = models.IntegerField('влажность утром')
     h_day = models.IntegerField('влажность днем')
     h_evening = models.IntegerField('влажность вечером')
     h_night = models.IntegerField('влажность ночью')
     h_average = models.IntegerField('средняя влажность за день')  
 
+    # def __str__(self):
+    #     return (self.id_district, self.date)
+
 class Pressure (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     pressure_morning = models.IntegerField('давление утром')
     pressure_day = models.IntegerField('давление днем')
     pressure_evening = models.IntegerField('давление вечером')
     pressure_night = models.IntegerField('давление ночью')
     pressure_average = models.IntegerField('среднее давление за день')    
 
+    # def __str__(self):
+    #     return (self.id_district, self.date)
+
 class Wind (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     w_morning = models.IntegerField('ветренность утром')
     w_day = models.IntegerField('ветренность днем')
     w_evening = models.IntegerField('ветренность вечером')
     w_night = models.IntegerField('ветренность ночью')
     w_average = models.IntegerField('средняя ветренность за день')   
 
+    # def __str__(self):
+    #     return (self.id_district, self.date)
+
 class Precipitation (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     p_morning = models.IntegerField('количество осадков утром')
     p_name_morning = models.CharField('тип осадков утром', max_length=100)
     p_day = models.IntegerField('количество осадков днем')
@@ -70,15 +90,21 @@ class Precipitation (models.Model):
     p_night = models.IntegerField('количество осадков ночью')
     p_name_night = models.CharField('тип осадков ночью', max_length=100)
     p_average = models.IntegerField('среднее количество осадков за день')  
-    p_name_average = models.CharField('средний тип осадков за день', max_length=100)    
+    p_name_average = models.CharField('средний тип осадков за день', max_length=100)   
+
+    # def __str__(self):
+    #     return (self.id_district, self.date) 
 
 class Precipitation_icon (models.Model):
     name = models.CharField('название типа осадков', max_length=100)
     icon = models.CharField('путь к иконке по типу осадков', max_length=100)  
 
-class Cloudy (models.Model):
+    def __str__(self):
+        return self.name 
+
+class Cloud (models.Model):
     id_district = models.IntegerField('id города')
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     c_morning = models.IntegerField('интенсивность облачности утром')
     c_name_morning = models.CharField('тип облачности утром', max_length=100)
     c_day = models.IntegerField('интенсивность облачности днем')
@@ -90,21 +116,29 @@ class Cloudy (models.Model):
     c_average = models.IntegerField('средняя интенсивность облачности за день')  
     c_name_average = models.CharField('средний тип облачности за день', max_length=100)   
 
+    # def __str__(self):
+    #     return (self.id_district, self.date) 
+
 class Cloudy_icon (models.Model):
     name = models.CharField('название типа облачности', max_length=100)
     icon = models.CharField('путь к иконке по типу облачности', max_length=100)
 
+    # def __str__(self):
+    #     return self.name
+
 class New (models.Model):
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     subject = models.CharField('тема новости', max_length=100)
     heading = models.TextField('заголовок новости')
     picture = models.CharField('путь к картинке', max_length=100)
     description = models.TextField('описание новости')
     source = models.CharField('источник новости', max_length=100)
+    # def __str__(self):
+    #     return (self.subject, self.heading) 
 
 class Day(models.Model):
     id_district = models.ForeignKey(District, on_delete = models.CASCADE)
-    date = models.DateTimeField('день')
+    date = models.DateField('день')
     news_id = models.ForeignKey(New, on_delete = models.CASCADE)
     id_temperature = models.ForeignKey(Temperature, on_delete = models.CASCADE)
     average_temperature = models.IntegerField('среднее значение температуры')
@@ -114,7 +148,7 @@ class Day(models.Model):
     average_precipitation = models.IntegerField('среднее значение осадков')
     name_precipitation = models.CharField('средний тип осадков', max_length=100) 
     icon_precipitation = models.CharField('путь к иконке по типу осадков', max_length=100)  
-    id_cloud = models.ForeignKey(Cloudy, on_delete = models.CASCADE)
+    id_cloud = models.ForeignKey(Cloud, on_delete = models.CASCADE)
     average_cloud = models.IntegerField('среднее значение облачности')
     name_cloud = models.CharField('средний тип облачности', max_length=100) 
     icon_cloud = models.CharField('путь к иконке по типу облачности', max_length=100)  
@@ -123,6 +157,10 @@ class Day(models.Model):
     id_wind = models.ForeignKey(Wind, on_delete = models.CASCADE)
     average_wind = models.IntegerField('среднее значение ветренности')
 
+    # def __str__(self):
+    #     return (self.id_district, self.date) 
+
+
 class User(models.Model):
     type = models.IntegerField('тип пользователя')
     name = models.CharField('имя пользователя', max_length=100)
@@ -130,9 +168,12 @@ class User(models.Model):
     patronymic = models.CharField('отчество пользователя', max_length=100)
     male = models.CharField('гендер пользователя', max_length=1)
     email = models.CharField('email пользователя', max_length=100)
-    birthday = models.DateTimeField('день рождения пользователя')
+    birthday = models.DateField('день рождения пользователя')
     login = models.CharField('логин пользователя', max_length=100)
     password = models.CharField('пароль пользователя', max_length=100)
+
+    def __str__(self):
+        return (self.name) 
 
 class Advertice (models.Model):
     subject = models.CharField('тема рекламного объявления', max_length=100)
@@ -140,3 +181,7 @@ class Advertice (models.Model):
     picture = models.CharField('путь к картинке', max_length=100)
     description = models.TextField('описание рекламного объявления')
     firm_name = models.CharField('рекламируемая фирма', max_length=100)
+
+    def __str__(self):
+        return (self.subject, self.heading, self.firm_name) 
+    
