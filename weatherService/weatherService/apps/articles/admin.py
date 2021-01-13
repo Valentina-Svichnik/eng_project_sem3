7e56ab9a5f4fa3d7c7e71f81c5c92d4cfdcbe379
Valentina Svichnik <svichnikvalentina@gmail.com>
2021-01-13 18:08:15 +0300

    added export

--- weatherService/weatherService/apps/articles/admin.py
+++ weatherService/weatherService/apps/articles/admin.py
@@ -2,5 +2,25 @@ from django.contrib import admin
 
 from .models import New, Advertice
 
-admin.site.register(New)
+from import_export.admin import ImportExportActionModelAdmin
+from import_export import resources
+from import_export import fields
+from import_export.widgets import ForeignKeyWidget
+
+# admin.site.register(New)
 admin.site.register(Advertice)
+
+class NewResource(resources.ModelResource):
+    id = fields.Field(column_name='id', attribute='id')
+    date = fields.Field(column_name='date', attribute='date')
+    subject = fields.Field(column_name='subject', attribute='subject')
+    heading = fields.Field(column_name='heading', attribute='heading')
+    picture = fields.Field(column_name='picture', attribute='picture')
+    description = fields.Field(column_name='description', attribute='description')
+    source = fields.Field(column_name='source', attribute='source')
+
+@admin.register(New)
+class NewAdmin(ImportExportActionModelAdmin):
+    resource_class = NewResource
+    list_display = ("id", "date", "subject", "heading", "picture", "description", "source")
+    list_filter = ("subject", ) 
--- weatherService/weatherService/settings.py
+++ weatherService/weatherService/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'import_export',
     'users.apps.UsersConfig',
     'articles.apps.ArticlesConfig',
     'weather.apps.WeatherConfig',
